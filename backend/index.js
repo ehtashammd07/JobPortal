45c7fb062497d51ceef758b7feb97a9c575df8dd
ehtashammd07 <mdehtasham165@gmail.com>
2025-06-21 12:58:21 +0530

    edit: added frontend domain for cors error

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://job-portal-five-silk.vercel.app/',  // Replace with your localhost for development
+    origin: 'https://job-portal-five-silk.vercel.app',  // Replace with your localhost for development
     credentials:true
 }
 
